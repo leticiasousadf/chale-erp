391a2e0257711fb722034b346806e80353ed1624
leticiasousadf <leticiasousadf@gmail.com>
2026-06-30 21:46:36 -0300

    Delete src directory

--- src/app/documentos/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = 'force-dynamic'
-import { createServerClient } from '@/lib/supabase-server'
-import { DocumentosClient } from '@/components/documentos/documentos-client'
-export default async function DocumentosPage() {
-  const supabase = createServerClient()
-  const { data: documentos } = await supabase.from('documentos').select('*').order('created_at', { ascending: false })
-  return <DocumentosClient documentos={documentos ?? []} />
-}
--- src/app/timeline/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = 'force-dynamic'
-import { createServerClient } from '@/lib/supabase-server'
-import { TimelineClient } from '@/components/timeline/timeline-client'
-export default async function TimelinePage() {
-  const supabase = createServerClient()
-  const { data: etapas } = await supabase.from('etapas_obra').select('*').order('previsao_inicio')
-  return <TimelineClient etapas={etapas ?? []} />
-}
